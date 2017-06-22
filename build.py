14290b52c5141abbb91c4d17bc2afafdf63dac03
sagarambalam <sagar.ambalam@gmail.com>
2017-06-22 09:41:32 +0000

    Done

--- build.py
+++ build.py
@@ -2,7 +2,5 @@ import pandas as pd
 import numpy as np
 
 
-def solution(data, index):
-    """
-    Enter your code here
-    """
+def solution(data, ind):
+    return pd.DataFrame(data, index=ind)
